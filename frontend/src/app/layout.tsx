6ddabdb7ec9105368224de7b15a8f2ddd914f840
kinn00kinn <haruki1009kk@gmail.com>
2025-11-04 11:50:11 +0900

    add: login

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import "./globals.css";
+import AuthProvider from "./components/AuthProvider";
 
 // --- ★ パンダSNSの基本情報を定義 ---
 const siteTitle = "PanDo (パンドゥ)";
@@ -64,7 +65,7 @@ export default function RootLayout({
       <body
         className={`${GeistSans.variable} ${GeistMono.variable} antialiased bg-white text-black`}
       >
-        {children}
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );
